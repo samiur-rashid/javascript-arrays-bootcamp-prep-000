0820623ac4bdbe71e13d8869adafb9362588beac
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 18:38:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,8 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   chocolateBars.pop()
   return chocolateBars
 }
+
+function removeElementFromEndOfArray(chocolateBars) {
+  chocolateBars.slice(0, chocolateBars.length - 1)
+  return chocolateBars
+}
