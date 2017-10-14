36415b2394a26684993faecc239862aeb86c1ac4
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 19:01:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
 }
 
 function removeElementFromBeginningOfArray(chocolateBars) {
-  chocolateBars.slice(1)
+  chocolateBars = chocolateBars.slice(1)
   return chocolateBars
 }
 
