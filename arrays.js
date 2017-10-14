b2c77173b80effd40bf937e5c3c154db2054c017
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 18:23:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(chocolateBars, element) {
-  
+
   chocolateBars.unshift(element)
-  
+
 }
