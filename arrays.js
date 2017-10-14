940922e178f14c2bd473ebb874c9943883c71434
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 18:08:56 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, newElement) {
+  chocolateBars.unshift(newElement)
+  return chocolateBars
+}
+
+function addElementToBeginningOfArray(chocolateBars, newElement) {
+  const chocolateBars
+  const newChocolateBars = [newElement, ...chocolateBars]
+}
+
+function accessElementInArray(n) {
+  return chocolateBars[n]
+}
