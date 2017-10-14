32615147cc8a5d2bc36b12a3a169d745b099e73e
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 18:29:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function accessElementInArray(chocolateBars,n) {
   return chocolateBars[n];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift()
+  return chocolateBars
+}
